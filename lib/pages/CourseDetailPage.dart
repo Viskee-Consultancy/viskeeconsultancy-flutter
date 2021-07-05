37e7f489fc78f35129d21eb44fd0d2d8c2c17eea
Zelong CHEN <zelong.chen@amadeus.com>
2021-07-05 20:44:27 +0200

    fix course detail layout

--- lib/pages/CourseDetailPage.dart
+++ lib/pages/CourseDetailPage.dart
@@ -43,198 +43,174 @@ class CourseDetailPage extends StatelessWidget {
                 image: DecorationImage(
                     image: AssetImage("images/background.jpg"),
                     fit: BoxFit.cover)),
-            child: Column(children: [
-              Expanded(
-                  flex: 1,
-                  child: Container(
-                    child: null,
-                  )),
-              Align(
-                  alignment: Alignment.center,
-                  child: Text(course!.name!,
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 30.0,
-                          color: CustomColors.GOLD))),
-              Expanded(
-                  flex: 1,
-                  child: Align(
-                      alignment: Alignment.topCenter,
-                      child: Padding(
-                        padding: EdgeInsets.all(10.0),
-                        child: Column(
-                          children: [
-                            Expanded(
-                              flex: 1,
-                              child: Text(
-                                "VET National Code:" + course!.vetCode!,
-                                style: TextStyle(color: Colors.white),
-                              ),
-                            ),
-                            Expanded(
-                              flex: 1,
-                              child: Text(
-                                "CRICOS Course Code:" + course!.cricosCode!,
-                                style: TextStyle(color: Colors.white),
-                              ),
-                            )
-                          ],
-                        ),
-                      ))),
-              Expanded(
-                  flex: 8,
-                  child: Align(
-                    alignment: Alignment.topCenter,
-                    child: Column(
-                      children: [
-                        Expanded(
-                            flex: 1,
-                            child: Column(
-                              children: [
-                                Expanded(
-                                    flex: 4,
-                                    child: Padding(
-                                      padding: EdgeInsets.all(20.0),
-                                      child: Image.asset(
-                                        "images/clock.png",
-                                        color: CustomColors.GOLD,
-                                      ),
-                                    )),
-                                Expanded(
-                                  flex: 1,
-                                  child: Text(
-                                    "Total Duration",
-                                    style: TextStyle(
-                                        fontWeight: FontWeight.bold,
-                                        fontSize: 18.0,
-                                        color: Colors.white),
-                                  ),
-                                ),
-                                Expanded(
-                                    flex: 1,
-                                    child: Padding(
-                                      padding: EdgeInsets.only(top: 2),
-                                      child: Text(
-                                        course!.duration!.toString() + "Weeks",
-                                        style: TextStyle(color: Colors.white),
-                                      ),
-                                    )),
-                                Expanded(
-                                    flex: 2,
-                                    child: Padding(
-                                      padding: EdgeInsets.only(top: 2),
-                                      child: Text(
-                                        course!.durationDetail!,
-                                        textAlign: TextAlign.center,
-                                        style: TextStyle(color: Colors.white),
-                                      ),
-                                    ))
-                              ],
-                            )),
-                        Expanded(
-                          flex: 1,
-                          child: Column(
-                            children: [
-                              Expanded(
-                                  flex: 4,
-                                  child: Padding(
-                                    padding: EdgeInsets.all(20.0),
-                                    child: Image.asset(
-                                      "images/location.png",
-                                      color: CustomColors.GOLD,
-                                    ),
-                                  )),
-                              Expanded(
-                                flex: 1,
-                                child: Text(
-                                  "Location",
-                                  style: TextStyle(
-                                      fontWeight: FontWeight.bold,
-                                      fontSize: 18.0,
-                                      color: Colors.white),
-                                ),
-                              ),
-                              Expanded(
-                                  flex: 1,
-                                  child: Padding(
-                                    padding: EdgeInsets.only(top: 5),
-                                    child: Text(
-                                      course!.location.toString(),
-                                      style: TextStyle(color: Colors.white),
-                                    ),
-                                  )),
-                              Expanded(
-                                flex: 1,
-                                child: Container(),
-                              )
-                            ],
-                          ),
-                        ),
-                        Expanded(
-                          flex: 1,
-                          child: Column(
-                            children: [
-                              Expanded(
-                                  flex: 4,
-                                  child: Padding(
-                                    padding: EdgeInsets.all(20.0),
-                                    child: Image.asset(
-                                      "images/pricing.png",
-                                      color: CustomColors.GOLD,
-                                    ),
-                                  )),
-                              Expanded(
-                                flex: 1,
-                                child: Text(
-                                  "Pricing",
-                                  style: TextStyle(
-                                      fontWeight: FontWeight.bold,
-                                      fontSize: 18.0,
-                                      color: Colors.white),
-                                ),
-                              ),
-                              Expanded(
-                                  flex: 1,
-                                  child: Padding(
-                                    padding: EdgeInsets.only(top: 2),
-                                    child: Text(
-                                      "Tuition Fee - OnShore Student Visa Holder: \$" +
-                                          course!.onshoreTuition!.toString(),
-                                      style: TextStyle(color: Colors.white),
-                                    ),
-                                  )),
-                              Expanded(
-                                  flex: 1,
-                                  child: Padding(
-                                    padding: EdgeInsets.only(top: 2),
-                                    child: Text(
-                                      "Tuition Fee - OffShore Int Student: \$" +
-                                          course!.offshoreTuition!.toString(),
-                                      style: TextStyle(color: Colors.white),
-                                    ),
-                                  ))
-                            ],
-                          ),
-                        ),
-                        Padding(
-                          padding: EdgeInsets.all(20.0),
-                          child: ElevatedButton(
-                              style: ButtonStyle(
-                                backgroundColor:
-                                    MaterialStateProperty.all<Color>(
-                                        CustomColors.GOLD),
-                              ),
-                              child: Text("Terms and Conditions"),
-                              onPressed: () {
-                                html.window.open(
-                                    "https://aibtglobal.edu.au/courses/terms-for-courses/",
-                                    "Terms and Conditions");
-                              }),
-                        )
-                      ],
-                    ),
-                  )),
-            ])));
+            child: ListView(shrinkWrap: true, children: _getListData())));
+  }
+
+  List<Widget> _getListData() {
+    List<Widget> widgets = [];
+    for (int i = 0; i < 3; i++) {
+      widgets.add(new ColumnItem(i));
+    }
+    return widgets;
+  }
+}
+
+class ColumnItem extends StatelessWidget {
+  late int index;
+  ColumnItem(int index) {
+    this.index = index;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (index == 0) {
+      return Align(
+          alignment: Alignment.center,
+          child: Text(course!.name!,
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 30.0,
+                  color: CustomColors.GOLD)));
+    } else if (index == 1) {
+      return Align(
+          alignment: Alignment.topCenter,
+          child: Padding(
+            padding: EdgeInsets.all(10.0),
+            child: Column(
+              children: [
+                Text(
+                  "VET National Code:" + course!.vetCode!,
+                  style: TextStyle(color: Colors.white),
+                ),
+                Text(
+                  "CRICOS Course Code:" + course!.cricosCode!,
+                  style: TextStyle(color: Colors.white),
+                ),
+              ],
+            ),
+          ));
+    } else if (index == 2) {
+      return Align(
+        alignment: Alignment.topCenter,
+        child: Column(
+          children: [
+            Column(
+              children: [
+                Padding(
+                  padding: EdgeInsets.all(20.0),
+                  child: Image.asset(
+                    "images/clock.png",
+                    height: 60,
+                    color: CustomColors.GOLD,
+                  ),
+                ),
+                Text(
+                  "Total Duration",
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 18.0,
+                      color: Colors.white),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(top: 2),
+                  child: Text(
+                    course!.duration!.toString() + "Weeks",
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(top: 2),
+                  child: Text(
+                    course!.durationDetail!,
+                    textAlign: TextAlign.center,
+                    style: TextStyle(color: Colors.white),
+                  ),
+                )
+              ],
+            ),
+            Column(
+              children: [
+                Padding(
+                  padding: EdgeInsets.all(20.0),
+                  child: Image.asset(
+                    "images/location.png",
+                    height: 60,
+                    color: CustomColors.GOLD,
+                  ),
+                ),
+                Text(
+                  "Location",
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 18.0,
+                      color: Colors.white),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(top: 5),
+                  child: Text(
+                    course!.location.toString(),
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+              ],
+            ),
+            Column(
+              children: [
+                Padding(
+                  padding: EdgeInsets.all(20.0),
+                  child: Image.asset(
+                    "images/pricing.png",
+                    height: 60,
+                    color: CustomColors.GOLD,
+                  ),
+                ),
+                Text(
+                  "Pricing",
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 18.0,
+                      color: Colors.white),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(top: 2),
+                  child: Text(
+                    "Tuition Fee - OnShore Student Visa Holder: \$" +
+                        course!.onshoreTuition!.toString(),
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(top: 2),
+                  child: Text(
+                    "Tuition Fee - OffShore Int Student: \$" +
+                        course!.offshoreTuition!.toString(),
+                    style: TextStyle(color: Colors.white),
+                  ),
+                )
+              ],
+            ),
+            Padding(
+              padding: EdgeInsets.all(20.0),
+              child: ElevatedButton(
+                  style: ButtonStyle(
+                    backgroundColor:
+                        MaterialStateProperty.all<Color>(CustomColors.GOLD),
+                  ),
+                  child: Text("Terms and Conditions"),
+                  onPressed: () {
+                    html.window.open(
+                        "https://aibtglobal.edu.au/courses/terms-for-courses/",
+                        "Terms and Conditions");
+                  }),
+            )
+          ],
+        ),
+      );
+    } else {
+      return new Container(
+        child: null,
+      );
+    }
   }
 }
