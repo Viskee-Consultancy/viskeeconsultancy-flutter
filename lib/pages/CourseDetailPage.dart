bbf113bb466e56bcea0592cdc11d9dff70f26e60
AibtGlobal <app.developer@aibtglobal.edu.au>
2021-10-26 23:08:44 +0200

    refactor course detail page

--- lib/pages/CourseDetailPage.dart
+++ lib/pages/CourseDetailPage.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
-import 'package:viskeeconsultancy/widgets/CommonWidgets.dart';
 import 'package:viskeeconsultancy/models/Course.dart';
 import 'package:viskeeconsultancy/values/CustomColors.dart';
-
-Course? _course;
+import 'package:viskeeconsultancy/widgets/CommonWidgets.dart';
 
 class CourseDetailPage extends StatelessWidget {
+  late Course _course;
+
   CourseDetailPage(Course input) {
     _course = input;
   }
@@ -24,131 +24,167 @@ class CourseDetailPage extends StatelessWidget {
 
   List<Widget> _getListData() {
     List<Widget> widgets = [];
-    for (int i = 0; i < 3; i++) {
-      widgets.add(new ColumnItem(i));
-    }
+    widgets.add(_buildCourseName());
+    widgets.add(_buildVetCode());
+    widgets.add(_buildCourseDetail());
     return widgets;
   }
-}
 
-class ColumnItem extends StatelessWidget {
-  late int _index;
-
-  ColumnItem(int index) {
-    this._index = index;
+  Widget _buildCourseName() {
+    return Align(
+        alignment: Alignment.center,
+        child: Padding(
+            padding: EdgeInsets.only(left: 5, right: 5),
+            child: Text(_course.name!,
+                textAlign: TextAlign.center,
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30.0, color: CustomColors.GOLD))));
   }
 
-  @override
-  Widget build(BuildContext context) {
-    if (_index == 0) {
-      return Align(
-          alignment: Alignment.center,
-          child: Padding(
-              padding: EdgeInsets.only(left: 5, right: 5),
-              child: Text(_course!.name!,
-                  textAlign: TextAlign.center,
-                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30.0, color: CustomColors.GOLD))));
-    } else if (_index == 1) {
-      return Align(
-          alignment: Alignment.topCenter,
-          child: Padding(
-            padding: EdgeInsets.all(10.0),
-            child: Column(
-              children: [getVetCodeText(_course), getCricosCodeText(_course)],
-            ),
-          ));
-    } else if (_index == 2) {
-      return Align(
+  Widget _buildVetCode() {
+    return Align(
         alignment: Alignment.topCenter,
-        child: Column(
-          children: [
-            Column(
-              children: [
-                Padding(
-                  padding: EdgeInsets.all(20.0),
-                  child: Icon(
-                    Icons.schedule_outlined,
-                    color: CustomColors.GOLD,
-                    size: 64,
-                  ),
-                ),
-                Text(
-                  "Total Duration",
-                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18.0, color: Colors.black),
-                ),
-                Padding(
-                  padding: EdgeInsets.only(top: 5),
-                  child: getTotalDurationText(_course),
-                ),
-                Padding(
-                  padding: EdgeInsets.only(top: 5, left: 10, right: 10),
-                  child: getDurationDetailText(_course),
-                )
-              ],
-            ),
-            Column(
-              children: [
-                Padding(
-                  padding: EdgeInsets.all(20.0),
-                  child: Icon(
-                    Icons.location_on_outlined,
-                    color: CustomColors.GOLD,
-                    size: 64,
-                  ),
-                ),
-                Text(
-                  "Location",
-                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18.0, color: Colors.black),
-                ),
-                Padding(
-                  padding: EdgeInsets.only(top: 5),
-                  child: getLocationText(_course),
-                ),
-              ],
-            ),
-            Column(
-              children: [
-                Padding(
-                  padding: EdgeInsets.all(20.0),
-                  child: Icon(
-                    Icons.attach_money_outlined,
-                    color: CustomColors.GOLD,
-                    size: 64,
-                  ),
-                ),
-                Text(
-                  "Pricing",
-                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18.0, color: Colors.black),
-                ),
-                Padding(
-                  padding: EdgeInsets.only(top: 5),
-                  child: getTuitionText(_course),
-                )
-              ],
-            ),
-            getPlacementWidget(_course),
-            Padding(
-              padding: EdgeInsets.all(20.0),
-              child: ElevatedButton(
-                  style: ButtonStyle(
-                    backgroundColor: MaterialStateProperty.all<Color>(CustomColors.GOLD),
-                  ),
-                  child: Text("Terms and Conditions"),
-                  onPressed: () {
-                    launchURL("https://aibtglobal.edu.au/courses/terms-for-courses/");
-                  }),
-            )
-          ],
+        child: Padding(
+          padding: EdgeInsets.all(10.0),
+          child: Column(
+            children: [_getVetCodeText(_course), _getCricosCodeText(_course)],
+          ),
+        ));
+  }
+
+  Widget _buildCourseDetail() {
+    return Align(
+      alignment: Alignment.topCenter,
+      child: Column(
+        children: [
+          _buildDurationSection(),
+          _buildLocationSection(),
+          _buildTuitionSection(),
+          _buildPlacementSection(),
+          _buildTermsConditionSection()
+        ],
+      ),
+    );
+  }
+
+  Widget _buildDurationSection() {
+    return Column(
+      children: [
+        Padding(
+          padding: EdgeInsets.all(20.0),
+          child: Icon(
+            Icons.schedule_outlined,
+            color: CustomColors.GOLD,
+            size: 64,
+          ),
         ),
+        Text(
+          "Total Duration",
+          style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18.0, color: Colors.black),
+        ),
+        Padding(
+          padding: EdgeInsets.only(top: 5),
+          child: _getTotalDurationText(_course),
+        ),
+        Padding(
+          padding: EdgeInsets.only(top: 5, left: 10, right: 10),
+          child: _getDurationDetailText(_course),
+        )
+      ],
+    );
+  }
+
+  Widget _buildLocationSection() {
+    return Column(
+      children: [
+        Padding(
+          padding: EdgeInsets.all(20.0),
+          child: Icon(
+            Icons.location_on_outlined,
+            color: CustomColors.GOLD,
+            size: 64,
+          ),
+        ),
+        Text(
+          "Location",
+          style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18.0, color: Colors.black),
+        ),
+        Padding(
+          padding: EdgeInsets.only(top: 5),
+          child: _getLocationText(_course),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildTuitionSection() {
+    return Column(
+      children: [
+        Padding(
+          padding: EdgeInsets.all(20.0),
+          child: Icon(
+            Icons.attach_money_outlined,
+            color: CustomColors.GOLD,
+            size: 64,
+          ),
+        ),
+        Text(
+          "Tuition",
+          style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18.0, color: Colors.black),
+        ),
+        Padding(
+          padding: EdgeInsets.only(top: 5),
+          child: _getTuitionText(_course),
+        )
+      ],
+    );
+  }
+
+  Widget _buildPlacementSection() {
+    if (_course != null && _course.placementFee != null) {
+      return Column(
+        children: [
+          Padding(
+            padding: EdgeInsets.all(20.0),
+            child: Icon(
+              Icons.work_outline_outlined,
+              color: CustomColors.GOLD,
+              size: 64,
+            ),
+          ),
+          Text(
+            "Placement",
+            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18.0, color: Colors.black),
+          ),
+          Padding(
+            padding: EdgeInsets.only(top: 5),
+            child: _getPlacementFeeText(_course),
+          ),
+          Padding(
+            padding: EdgeInsets.only(top: 5, left: 10, right: 10),
+            child: _getPlacementDurationText(_course),
+          )
+        ],
       );
     } else {
-      return new Container(
-        child: null,
-      );
+      return Container();
     }
   }
 
-  Text getVetCodeText(Course? course) {
+  Widget _buildTermsConditionSection() {
+    return Padding(
+      padding: EdgeInsets.all(20.0),
+      child: ElevatedButton(
+          style: ButtonStyle(
+            backgroundColor: MaterialStateProperty.all<Color>(CustomColors.GOLD),
+          ),
+          child: Text("Terms and Conditions"),
+          onPressed: () {
+            _launchURL("https://aibtglobal.edu.au/courses/terms-for-courses/");
+          }),
+    );
+  }
+
+  Text _getVetCodeText(Course? course) {
     if (course != null && course.vetCode != null) {
       return Text(
         "VET National Code:" + course.vetCode!,
@@ -159,7 +195,7 @@ class ColumnItem extends StatelessWidget {
     }
   }
 
-  Text getCricosCodeText(Course? course) {
+  Text _getCricosCodeText(Course? course) {
     if (course != null && course.cricosCode != null) {
       return Text(
         "CRICOS Course Code:" + course.cricosCode!,
@@ -170,7 +206,7 @@ class ColumnItem extends StatelessWidget {
     }
   }
 
-  Widget getTotalDurationText(Course? course) {
+  Widget _getTotalDurationText(Course? course) {
     if (course != null) {
       if (course.isOnPromotion && course.promotionDuration != null) {
         return Column(
@@ -196,7 +232,7 @@ class ColumnItem extends StatelessWidget {
     }
   }
 
-  Widget getDurationDetailText(Course? course) {
+  Widget _getDurationDetailText(Course? course) {
     if (course != null && course.durationDetail != null) {
       if (course.isOnPromotion && course.promotionDurationDetail != null) {
         return Column(
@@ -225,7 +261,7 @@ class ColumnItem extends StatelessWidget {
     }
   }
 
-  Widget getLocationText(Course? course) {
+  Widget _getLocationText(Course? course) {
     if (course != null && course.location != null) {
       if (course.isOnPromotion && course.promotionLocation != null) {
         return Column(
@@ -254,7 +290,7 @@ class ColumnItem extends StatelessWidget {
     }
   }
 
-  Widget getTuitionText(Course? course) {
+  Widget _getTuitionText(Course? course) {
     if (course != null && course.tuition != null) {
       if (course.isOnPromotion && course.promotionTuition != null) {
         return Column(
@@ -283,38 +319,7 @@ class ColumnItem extends StatelessWidget {
     }
   }
 
-  Widget getPlacementWidget(Course? course) {
-    if (course != null && course.placementFee != null) {
-      return Column(
-        children: [
-          Padding(
-            padding: EdgeInsets.all(20.0),
-            child: Icon(
-              Icons.work_outline_outlined,
-              color: CustomColors.GOLD,
-              size: 64,
-            ),
-          ),
-          Text(
-            "Placement",
-            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18.0, color: Colors.black),
-          ),
-          Padding(
-            padding: EdgeInsets.only(top: 5),
-            child: getPlacementFeeText(course),
-          ),
-          Padding(
-            padding: EdgeInsets.only(top: 5, left: 10, right: 10),
-            child: getPlacementDurationText(course),
-          )
-        ],
-      );
-    } else {
-      return Container();
-    }
-  }
-
-  Widget getPlacementFeeText(Course? course) {
+  Widget _getPlacementFeeText(Course? course) {
     if (course != null && course.placementFee != null) {
       return Text(
         "Placement Fee: " + course.placementFee!.toString() + " \$",
@@ -326,7 +331,7 @@ class ColumnItem extends StatelessWidget {
     }
   }
 
-  Widget getPlacementDurationText(Course? course) {
+  Widget _getPlacementDurationText(Course? course) {
     if (course != null && course.placementDuration != null) {
       return Text(
         "Placement Duration: " + course.placementDuration!.toString() + " hours",
@@ -338,7 +343,7 @@ class ColumnItem extends StatelessWidget {
     }
   }
 
-  launchURL(String url) async {
+  _launchURL(String url) async {
     if (await canLaunch(url)) {
       await launch(url, forceWebView: true);
     } else {
