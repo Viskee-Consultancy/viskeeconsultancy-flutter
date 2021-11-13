c881a07d34ce8a0f0daa2512e85e427cf545c8e6
AibtGlobal <app.developer@aibtglobal.edu.au>
2021-11-13 19:03:21 +0100

    clean flutter main file

--- lib/main.dart
+++ lib/main.dart
@@ -9,8 +9,7 @@ import 'package:viskeeconsultancy/values/StringConstants.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
-  SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual,
-      overlays: [SystemUiOverlay.bottom, SystemUiOverlay.top]);
+  SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual, overlays: [SystemUiOverlay.bottom, SystemUiOverlay.top]);
   runApp(const MyApp());
 }
 
@@ -21,70 +20,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     precacheImage(AssetImage("images/background.png"), context);
-    // return FutureBuilder(
-    //   future: Init.instance.initialize(),
-    //   builder: (context, AsyncSnapshot snapshot) {
-    //     // Show splash screen while waiting for app resources to load:
-    //     if (snapshot.connectionState == ConnectionState.waiting) {
-    //       return const MaterialApp(home: Splash());
-    //     } else {
-          // Loading is done, return the app:
-          return MaterialApp(
-            title: StringConstants.APP_NAME,
-            theme: ThemeData(
-              // This is the theme of your application.
-              //
-              // Try running your application with "flutter run". You'll see the
-              // application has a blue toolbar. Then, without quitting the app, try
-              // changing the primarySwatch below to Colors.green and then invoke
-              // "hot reload" (press "r" in the console where you ran "flutter run",
-              // or simply save your changes to "hot reload" in a Flutter IDE).
-              // Notice that the counter didn't reset back to zero; the application
-              // is not restarted.
-              // primarySwatch: Colors.grey,
-                fontFamily: 'DM'
-            ),
-            home: OnshoreOffshorePage(),
-          );
-        // }
-    //   },
-    // );
-  }
-}
-
-class Splash extends StatelessWidget {
-  const Splash({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    bool lightMode =
-        MediaQuery.of(context).platformBrightness == Brightness.light;
-    return Scaffold(
-      backgroundColor:
-      lightMode ? const Color(0xffffffff) : const Color(0xff000000),
-      body: Center(
-          child: lightMode
-              ? Image.asset('images/vc_logo_landscape.png')
-              : Image.asset('images/vc_logo_landscape_white.png')),
+    return MaterialApp(
+      title: StringConstants.APP_NAME,
+      theme: ThemeData(fontFamily: 'DM'),
+      home: OnshoreOffshorePage(),
     );
   }
 }
-
-class Init {
-  Init._();
-  static final instance = Init._();
-
-  Future initialize() async {
-    // This is where you can initialize the resources needed by your app while
-    // the splash screen is displayed.  Remove the following example because
-    // delaying the user experience is a bad design practice!
-    await Future.delayed(const Duration(seconds: 2));
-  }
-}
-
-// void main() => runApp(MaterialApp(
-//         theme: ThemeData(fontFamily: 'DM'),
-//         home: OnshoreOffshorePage(),
-//         routes: <String, WidgetBuilder>{
-//           '/main_page': (BuildContext context) => OnshoreOffshorePage(),
-//         }));
